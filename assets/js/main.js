b31f146d7f0d8dc90dec238371c05ca1d3e2b801
J03l-code <joeljiyane@gmail.com>
2026-05-10 23:54:45 -0500

    UI: Integrated profile photo next to 'Mi Portal' text in header

--- assets/js/main.js
+++ assets/js/main.js
@@ -158,7 +158,7 @@ async function updateAuthUI() {
         : `<svg viewBox="0 0 24 24" width="18" height="18" fill="none" stroke="currentColor" stroke-width="2"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" /><circle cx="12" cy="7" r="4" /></svg>`;
 
       portalBtn.innerHTML = `
-        <div class="profile-avatar-container">
+        <div class="profile-portal-widget">
           <div class="profile-avatar-mini">${avatarHtml}</div>
           <span class="profile-label">Mi Portal</span>
           <div class="profile-dropdown">
