0af9b543bc4be52f91f5fc4c67a2f05c1af4bbbc
J03l-code <joeljiyane@gmail.com>
2026-05-18 12:57:08 -0500

    fix: romper caché del navegador (v=100.0) para redimensionar los iconos dispersos

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ header("Expires: 0");
 
   <!-- CSS -->
   <link rel="stylesheet" href="assets/css/main.css?v=61.0">
-  <link rel="stylesheet" href="assets/css/components.css?v=61.0">
+  <link rel="stylesheet" href="assets/css/components.css?v=100.0">
   <link rel="stylesheet" href="assets/css/animations.css?v=61.0">
   
   <style>
