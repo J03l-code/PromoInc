6b57a1d009bec91c733f86820472c4fb80abafe7
J03l-code <joeljiyane@gmail.com>
2026-05-18 14:47:00 -0500

    style: change nav-categories layout from horizontal scroll overflow to flexible flex-wrap to prevent category names from being cut off

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ header("Expires: 0");
 
   <!-- CSS -->
   <link rel="stylesheet" href="assets/css/main.css?v=61.0">
-  <link rel="stylesheet" href="assets/css/components.css?v=102.0">
+  <link rel="stylesheet" href="assets/css/components.css?v=103.0">
   <link rel="stylesheet" href="assets/css/animations.css?v=61.0">
   
   <style>
